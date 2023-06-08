55397e9f7151706b9d94f3cb9e1909f1d7737606
adil koç <118297248+adilkoc@users.noreply.github.com>
2023-06-08 14:21:39 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+let screen = document.querySelector('.screen');
+let buttons = document.querySelectorAll('.button');
+let equal = document.querySelector('.btn-green');
+let clear = document.querySelector('.btn-red');
+
+buttons.forEach(function(button) {
+  button.addEventListener('click', function(e) {
+    let value = e.target.dataset.num;
+    screen.value += value;
+  });
+});
+
+equal.addEventListener('click', function(e) {
+  if (screen.value === '') {
+    screen.value = '';
+  } else {
+    let answer = eval(screen.value);
+    screen.value = answer;
+  }
+});
+
+clear.addEventListener('click', function(e) {
+  screen.value = '';
+});
